f9ba29bc78b9987cfe9b4cbca4831f37ba7dfc6e
xssxss-web <2896533099@qq.com>
2026-06-26 20:52:21 +0800

    Update chunkMerge.js

--- functions/upload/chunkMerge.js
+++ functions/upload/chunkMerge.js
@@ -1,7 +1,7 @@
 /* ========== 分块合并处理 ========== */
 import { createResponse, getUploadIp, getIPAddress, selectConsistentChannel, buildUniqueFileId, endUpload, sanitizeUploadFolder } from './uploadTools';
 import { retryFailedChunks, cleanupFailedMultipartUploads, checkChunkUploadStatuses, cleanupChunkData, cleanupUploadSession } from './chunkUpload';
-import { S3Client, CompleteMultipartUploadCommand } from "@aws-sdk/client-s3";
+ 
 import { getDatabase } from '../utils/databaseAdapter.js';
 import { fetchPageConfig } from '../utils/sysConfig.js';
 
